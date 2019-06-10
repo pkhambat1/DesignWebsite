179312fbefa208cd25cb67d35b5ee468fce170ec
pkhambat1 <pkhambat@risd.edu>
2019-06-10 23:57:02 +0530

    arrows and carousel dot-lines refined

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ function scrollFunction() {
 		$('.navbar-light').css("padding", "0px 10px");
 		$('.navbar-light').css("opacity", ".96");
 		$('.navbar-light').css("background", "#fff");
-		$('.portfolio-navbar.navbar').css("box-shadow", "0 4px 10px rgba(0,0,0,.1)");
+		$('.portfolio-navbar.navbar').css("box-shadow", "0 4px 10px rgba(0,0,0,.15)");
 	} else {	
 		$('#logo, .portfolio-navbar .navbar-nav .nav-link').css({"font-size" : ""});
 		$('#logo, .portfolio-navbar .navbar-nav .nav-link').css({"color" : ""});
@@ -48,8 +48,20 @@ $(document).ready(function() {
 		// navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
 		// navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
 		// navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"] 
-		navText: ["<div class=\"carousel-control-prev\" role=\"button\" data-slide=\"prev\"><div class='arrowbox'><i class='fa fa-angle-left align-middle mt-3 mb-3'></i></div><span class=\"sr-only\">Previous</span></div>",
-		"<div class=\"carousel-control-next\" data-slide=\"next\"><div class='arrowbox'><i class='fa fa-angle-right align-middle mt-3 mb-3'></i></div><span class=\"sr-only\">Next</span></div>"]
+		navText: ["<div class=\"carousel-control-prev\" role=\"button\" data-slide=\"prev\"><div class=\"arrowbox\"><i class=\"fa fa-angle-left align-middle mt-4 mb-4 \"></i></div><span class=\"sr-only\">Previous</span></div>",
+		"<div class=\"carousel-control-next\" data-slide=\"next\"><div class='arrowbox'><i class='fa fa-angle-right align-middle mt-4 mb-4 arrowright'></i></div><span class=\"sr-only\">Next</span></div>"]
 	});
 
+});
+
+$(document).ready(function() {
+	$('.arrowbox, .owl-dots').css("opacity", "0");
+	$('.rect').hover(function(){
+      	$(this).find('.arrowbox').css("opacity", ".4");
+      	$(this).find('.owl-dots').css("opacity", "1");
+  	}, 
+	function() {
+  		$(this).find('.arrowbox').css("opacity", "0");
+  		$(this).find('.owl-dots').css("opacity", "0");
+	});
 });
