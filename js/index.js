2ee4ea1aaa3bdd5ff8e4ffa001ec4b1038768610
Pezanne Khambatta <39662781+pkhambat1@users.noreply.github.com>
2019-06-14 18:34:39 +0530

    lots

--- js/index.js
+++ js/index.js
@@ -1,9 +1,25 @@
 // Global vars
 
 var toggleVal = "initial";
+var itemCount = 2;
+
+// Being extremely nitpicky here
+$(window).on("resize", function(event){
+  	windowWidth = $(this).width();
+  	if ($(this).width() >= 992 && toggleVal == "uncollapsed") {
+  		console.log("if hit!");
+  		toggleVal = "stretchUncollapsed";
+  	} else if ($(this).width() < 992 && toggleVal == "stretchUncollapsed") {
+  		console.log("else if hit!");
+  		toggleVal = "uncollapsed";
+  	}
 
 
-window.onload = function() {
+  	console.log($(this).width());
+  	console.log(toggleVal);
+});
+
+$(window).onload = function() {
     document.body.className += " loaded";
 }
 
@@ -29,10 +45,15 @@ function navbarChange(toggleVal) {
 	// if scrolled down
 	if ($(this).scrollTop() > $('.fixed-top').height()) {
 		$('#logo, .portfolio-navbar .navbar-nav .nav-link').css({"color" : "#212529"});
-		$('.navbar-light').css("padding", "0px 10px");
 		$('.navbar-light').css("background", "#fff");
 		$('.navbar-light').css("opacity", ".96");
-		if (toggleVal == "collapsed" || toggleVal == "initial") {
+		if (toggleVal == "uncollapsed") {
+			$('.navbar-light').css("padding", "0px 10px 10px");
+		} else {
+			$('.navbar-light').css("padding", "0px 10px");
+		}
+
+		if (toggleVal == "collapsed" || toggleVal == "initial" || toggleVal == "stretchUncollapsed") {
 			$('.portfolio-navbar.navbar').css("box-shadow", "0 4px 10px rgba(0,0,0,.15)");
 		}
 	// if on top 
@@ -42,7 +63,7 @@ function navbarChange(toggleVal) {
 			$('.navbar-light').css({"padding" : ""});
 			$('.navbar-light').css({"background" : ""});
 			$('.navbar-light').css({"opacity" : ""});
-		if(toggleVal == "collapsed" || toggleVal == "initial"){
+		if(toggleVal == "collapsed" || toggleVal == "initial" || toggleVal == "stretchUncollapsed"){
 			$('.portfolio-navbar.navbar').css({"box-shadow" : ""});
 		}
 	}
@@ -52,6 +73,11 @@ function navbarChange(toggleVal) {
 $(document).on('scroll', function() {
 	navbarChange(toggleVal);
 });
+
+$(document).on('resize', function() {
+	navbarChange(toggleVal);
+});
+
 
 $(document).ready(function() {
 
@@ -67,6 +93,7 @@ $(document).ready(function() {
 			$('.navbar').css({"box-shadow" : ""});
 		}
 		toggleVal = "collapsed";
+		console.log(toggleVal);
 	});
 
 	// If uncollapsed
@@ -74,6 +101,7 @@ $(document).ready(function() {
 		// Add shadow
 		$(this).css("box-shadow", "0 2px 10px rgba(0,0,0,.15)");
 		toggleVal = "uncollapsed";
+		console.log(toggleVal);
 	});
 });
 
@@ -82,16 +110,25 @@ $(document).ready(function() {
 $(document).ready(function() {
 
 	$('.owl-carousel').owlCarousel({
-		items:1,
+		items:itemCount,
 		margin:0,
 		autoHeight:true,
-		mouseDrag:false,
+		// mouseDrag:false,
 		autoplay:false,
 		nav:true,
 		lazyLoad:true,
-		navText: 	["<div class=\"carousel-control-prev\" role=\"button\" data-slide=\"prev\"><div class=\"arrowbox\"><i class=\"fa fa-angle-left align-middle mt-4 mb-4 \"></i></div><span class=\"sr-only\">Previous</span></div>",
+		responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        768:{
+	            items:2
+	        }
+	    },
+		navText: ["<div class=\"carousel-control-prev\" role=\"button\" data-slide=\"prev\"><div class=\"arrowbox\"><i class=\"fa fa-angle-left align-middle mt-4 mb-4 \"></i></div><span class=\"sr-only\">Previous</span></div>",
 					"<div class=\"carousel-control-next\" role=\"button\" data-slide=\"next\"><div class=\"arrowbox\"><i class=\"fa fa-angle-right align-middle mt-4 mb-4 \"></i></div><span class=\"sr-only\">Next</span></div>"]
-	});
+	});	
 
 });
 
