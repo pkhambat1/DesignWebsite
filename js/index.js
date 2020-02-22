e7aef1e3b545df87706976ab7cbf3fd1d4e669dc
Pezanne Khambatta <pkhambat@risd.edu>
2020-02-22 01:18:22 -0800

    js/css removed from gitignore. No more uglified

---
+++ js/index.js
@@ -0,0 +1,81 @@
+// Global vars
+var toggleVal = "initial";
+
+$(document).ready(() => {
+    typing();
+});
+
+
+function typing(){
+    $("#typing").typed({
+        // strings: ["", "", "", "", " with design...", "", " with software...", "", " with code..."],
+        strings: ["", "", "", "", "", "", "", "", "and that's because I simply love building things!"],
+        backSpeed: 30,
+        typeSpeed: 30,
+        loop: false
+    });
+};
+
+// Being extremely nitpicky here
+$(window).on("resize", function(){
+    if ($(this).width() >= 992 && toggleVal == "uncollapsed") {
+        toggleVal = "stretchUncollapsed";
+        navbarChange("stretchUncollapsed");
+    } else if ($(this).width() < 992 && toggleVal == "stretchUncollapsed") {
+        toggleVal = "uncollapsed";
+        navbarChange("uncollapsed");
+    }
+});
+
+var navbarChange = function (toggleVal) {
+    // if scrolled down
+    if ($(this).scrollTop() > $('.fixed-top').height()) {
+        $('.navbar-light').css("background", "rgba(255, 255, 255, 0.96)");
+        $('.portfolio-navbar.navbar').css("box-shadow", "0 4px 10px rgba(0,0,0,.15)");
+        if (toggleVal === "uncollapsed") {
+            $('.navbar-light').css("padding", "0px 5px 10px");
+        } else {
+            $('.navbar-light').css("padding", "0px 5px");
+        }
+        
+        if (toggleVal === "collapsed" || toggleVal === "initial") {
+            $('.portfolio-navbar.navbar').css("box-shadow", "0 4px 10px rgba(0,0,0,.15)");
+        }
+        // if on top 
+    } else {
+        $('#logo, .portfolio-navbar .navbar-nav .nav-link').css({ "font-size": "" });
+        $('.navbar-light').css({ "padding": "" });
+        $('.navbar-light').css({ "background": "" });
+        $('.navbar-light').css({ "opacity": "" });
+        if (toggleVal === "collapsed" || toggleVal === "initial" || toggleVal === "stretchUncollapsed") {
+            $('.portfolio-navbar.navbar').css({ "box-shadow": "" });
+        } else {
+            $('.portfolio-navbar.navbar').css("box-shadow", "0 4px 10px rgba(0,0,0,.15)");
+        }
+    }
+}
+
+// If scrolled
+$(document).on('scroll', () => {
+    navbarChange(toggleVal);
+});
+
+$(document).ready(() => {
+    
+    // If collapsed
+    $('.navbar').on('hide.bs.collapse', () => {
+        toggleVal = "collapsed";
+        navbarChange("collapsed");
+    });
+    
+    // If uncollapsed
+    $('.navbar').on('show.bs.collapse', () => {
+        toggleVal = "uncollapsed";
+        navbarChange("uncollapsed");
+    });
+});
+
+
+
+
+
