80203eea272f8afb374f1e12f10ebaa58f42fbe2
Pezanne Khambatta <pkhambat@risd.edu>
2020-02-23 23:03:45 -0800

    some js edits, navbar fix, tech & design on one line now

--- js/content-carousel.js
+++ js/content-carousel.js
@@ -6,7 +6,11 @@ $(document).ready(() => {
 
 var scrollCarousel = () => {
     $('.carousel-control-prev, .carousel-control-next, .owl-dots').on('click', function () {
-        $('html,body').animate({ scrollTop: $(this).closest('.rect').offset().top - 10 }, 400);
+        // prevent overclicking glitch
+        if(!$(this).closest('.rect').find('button.owl-next').hasClass('disabled')) {
+            console.log($(this).closest('.rect').find('button.owl-next'))
+            $('html,body').animate({ scrollTop: $(this).closest('.rect').offset().top - 10 }, 400);
+        }
     });
 };
 
@@ -43,8 +47,20 @@ var loadCarousel = () => {
                 items: 2
             }
         },
-        navText: ["<div class=\"carousel-control-prev\" role=\"button\" data-slide=\"prev\"><div class=\"arrowbox\"><i class=\"fa fa-angle-left align-middle mt-4 mb-4 \"></i></div><span class=\"sr-only\">Previous</span></div>",
-        "<div class=\"carousel-control-next\" role=\"button\" data-slide=\"next\"><div class=\"arrowbox\"><i class=\"fa fa-angle-right align-middle mt-4 mb-4 \"></i></div><span class=\"sr-only\">Next</span></div>"]
+        navText: [
+            `<div class="carousel-control-prev" role="button" data-slide="prev">
+                <div class="arrowbox">
+                    <i class="fa fa-angle-left align-middle mt-4 mb-4"></i>
+                </div>
+                <span class="sr-only">Previous</span>
+            </div>`,
+            `<div class="carousel-control-next" role="button" data-slide="next">
+                <div class="arrowbox">
+                    <i class="fa fa-angle-right align-middle mt-4 mb-4"></i>
+                </div>
+                <span class="sr-only">Next</span>
+            </div>`
+        ]
     }).on("dragged.owl.carousel", function () {
         $('html,body').animate({ scrollTop: $(this).closest('.rect').offset().top - 10 }, 400);
     });
@@ -88,6 +104,7 @@ var slideCarousel = () => {
             function () {
                 $(this).find('.arrowbox').css({ "opacity": "" });
                 $(this).find('.owl-dots').css({ "opacity": "" });
+                console.log('yeet');
             });
             // Hover has 0 effect
             $(this).closest('.rect:hover').css("top", "0");
